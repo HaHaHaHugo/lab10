116e4b8bdcf3ecf078e5bce347319f18892e801b
HaHaHaHugo <Navapol_naksen@cmu.ac.th>
2023-12-19 16:14:32 +0700

    lab11_3.cpp completed

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,30 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include<iostream>
+#include<iomanip>
+#include<cmath>
+#include<fstream>
+#include<string>
+using namespace std; 
+
+int main(){
+    ifstream source;
+    source.open("score.txt");
+    string data;
+    double n,sum,mean;
+    while(getline(source,data)){
+		sum += atof(data.c_str());
+        n++;
+	}
+    mean = sum/n;
+    source.close();
+    source.open("score.txt");
+    double stande,sumsd;
+    while(getline(source,data)){
+		sumsd += pow(atof(data.c_str())-mean,2);
+	}
+    stande = sqrt(sumsd/n);
+    source.close();
+    cout << "Number of data = " << n << endl;
+    cout << setprecision(3);
+    cout << "Mean = " << mean << endl;
+    cout << "Standard deviation = " << stande;
+}
